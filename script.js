aa256211cb8b9e02b42a2006b1e502af2fdac4a9
caylan wilcox <caylan45@yahoo.com>
2022-11-16 16:13:10 -0600

    new change

--- script.js
+++ script.js
@@ -2,40 +2,34 @@ const userCardTemplate =document.querySelector("[data-user-template]")
 const userCardContainer = document.querySelector("[data-user-cards-container]")
 const searchInput = document.querySelector("[data-search]")
 
-let users = []
+let cards = []
 
 searchInput.addEventListener("input", e =>{ 
-  const value = e.target.value.toLowerCase();
-  users.forEach(user =>{
-  const isVisible=user.name.toLowerCase().includes(value) || user.email.toLowerCase().includes(value)
+  
+  const value = e.target.value
+  cards.forEach(user =>{
+
+  const isVisible=user.first_name.includes(value) || user.last_name.toLowerCase().includes(value)
+ 
   user.element.classList.toggle("hide", !isVisible)
+  
 })
 })
-fetch("https://jsonplaceholder.typicode.com/users")
+fetch("https://www.balldontlie.io/api/v1/players")
 .then(res => res.json())
 .then(data => {
-  users = data.map(user=> {
+console.log(data)
+  cards = data.data.map(user=> {
 const card = userCardTemplate.content.cloneNode(true).children[0]
 const header = card.querySelector("[data-header]")
 const body = card.querySelector("[data-body]")
-header.textContent = user.name
-body.textContent = user.email
+const footer=card.querySelector("[data-footer]")
+header.textContent = user.first_name
+body.textContent = user.last_name
+footer.textContent = user.position
 userCardContainer.append(card)
-return { name:user.name, email:user.email, element:card }
+
+return { firstname:user.first_name, lastname:user.last_name , element:card }
 
 })  
   })
-
-
-const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': 'cfe114add4mshccfcf292350eb2ep16d0b9jsn3da07f460d7f',
-		'X-RapidAPI-Host': 'free-nba.p.rapidapi.com'
-	}
-};
-
-fetch('https://free-nba.p.rapidapi.com/players?page=0&per_page=25', options)
-	.then(response => response.json())
-	.then(response => console.log(response))
-	.catch(err => console.error(err))
